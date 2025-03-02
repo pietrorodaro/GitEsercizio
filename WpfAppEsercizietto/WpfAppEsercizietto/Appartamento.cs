3b4c7417e4bc1984a3e16495bdac6047ae2055f0
Pietro Rodaro <11pietro.rodaro@gmail.com>
2025-03-02 21:23:52 +0100

    Implementazione classe appartamento

--- WpfAppEsercizietto/WpfAppEsercizietto/Appartamento.cs
+++ WpfAppEsercizietto/WpfAppEsercizietto/Appartamento.cs
@@ -8,5 +8,18 @@ namespace WpfAppEsercizietto
 {
     public class Appartamento
     {
+        private int civico;
+        private string tipo;
+
+        public int Civico { get { return civico; } set { civico = value; } }
+        public string Tipo { get { return tipo; } set { tipo = value; } }
+
+        public Appartamento(int civico, string tipo) 
+        {
+            this.civico = civico;
+            this.tipo = tipo;
+        }
+
+
     }
 }
