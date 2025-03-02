bdf1e56a3f4faf2cca181ca003d397f05b555ffe
Pietro Rodaro <11pietro.rodaro@gmail.com>
2025-03-02 21:14:50 +0100

    Implementazione dei setter e getter

--- WpfAppEsercizietto/WpfAppEsercizietto/Cliente.cs
+++ WpfAppEsercizietto/WpfAppEsercizietto/Cliente.cs
@@ -8,5 +8,12 @@ namespace WpfAppEsercizietto
 {
     public class Cliente
     {
+        string nome, cognome, cod_Fis;
+
+        public string Nome { get {  return nome; } set { nome = value; } }
+        public string Cognome { get { return cognome; } set { cognome = value; } }
+        public string Cod_Fis { get { return cod_Fis; } set { cod_Fis = value; } }
+
+        
     }
 }
